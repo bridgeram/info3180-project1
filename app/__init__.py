0e8f4098bd66f14a16a3a78fc93fbce71675a93f
Kyle Ramgolam <kyle.ramgolam@gmail.com>
2018-03-10 17:10:28 +0000

    first

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask
 
-# Config Values
-USERNAME = 'admin'
-PASSWORD = 'password123'
+
 
 # SECRET_KEY is needed for session security, the flash() method in this case stores the message in a session
 SECRET_KEY = 'Sup3r$3cretkey'
