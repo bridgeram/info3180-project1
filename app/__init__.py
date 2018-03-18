7faec086da7f796cc54708fbee6a48ab3714d099
Kyle Ramgolam <kyle.ramgolam@gmail.com>
2018-03-18 22:42:15 +0000

    aids

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ UPLOAD_FOLDER = './app/static/uploads'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "gaza"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://kyzer:EralixBain@localhost/project1"
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql://awjlxxdgzsvced:34f93e2e57d7088421cb58704f5b840d28cd76a3d5bd46282b5a185f265d0b6d@ec2-54-204-44-140.compute-1.amazonaws.com:5432/dantits6675eai'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 
